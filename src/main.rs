12c9ef7b3ee497c27caa698b614128f60228a9ba
adrocodes <adrocodes@users.noreply.github.com>
2023-02-20 19:59:44 +1100

    feat: update from vertices approach

--- src/main.rs
+++ src/main.rs
@@ -17,36 +17,29 @@ fn main() {
 
     dbg!(&graph);
 
-    let graph = GraphBuilder::<String>::new()
-        .from_vertices(vec![
-            "A".into(),
-            "B".into(),
-            "C".into(),
-            "D".into(),
-            "E".into(),
-            "F".into(),
-        ])
-        .insert_edge("A".into(), "B".into())
-        .insert_edge("A".into(), "D".into())
-        .insert_edge("A".into(), "E".into())
-        .insert_edge("B".into(), "C".into())
-        .insert_edge("D".into(), "E".into())
-        .insert_edge("E".into(), "F".into())
-        .insert_edge("E".into(), "C".into())
-        .insert_edge("C".into(), "F".into())
-        .build();
+    let graph = GraphBuilder::<String>::from_vertices(vec![
+        "A".into(),
+        "B".into(),
+        "C".into(),
+        "D".into(),
+        "E".into(),
+        "F".into(),
+    ])
+    .insert_edge("A".into(), "B".into())
+    .insert_edge("A".into(), "D".into())
+    .insert_edge("A".into(), "E".into())
+    .insert_edge("B".into(), "C".into())
+    .insert_edge("D".into(), "E".into())
+    .insert_edge("E".into(), "F".into())
+    .insert_edge("E".into(), "C".into())
+    .insert_edge("C".into(), "F".into())
+    .build();
 
     dbg!(&graph);
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 struct Node<T>(T);
-
-#[derive(Debug, Hash, Eq, PartialEq)]
-struct Edge<T> {
-    from: Node<T>,
-    to: Node<T>,
-}
 
 #[derive(Debug)]
 struct GraphBuilder<T: PartialEq + Eq + Hash + Clone> {
@@ -71,39 +64,49 @@ impl<T: PartialEq + Eq + Hash + Clone> GraphBuilder<T> {
         GraphBuilder::default()
     }
 
-    fn insert_node(mut self, node: T) -> GraphBuilder<T> {
-        self.vertices.insert(node, HashSet::new());
+    fn _insert_node(builder: &mut GraphBuilder<T>, node: T) {
+        builder.vertices.insert(node, HashSet::new());
+    }
+
+    pub fn insert_node(mut self, node: T) -> GraphBuilder<T> {
+        GraphBuilder::<T>::_insert_node(&mut self, node);
         self
     }
 
-    fn insert_edge(mut self, from: T, to: T) -> GraphBuilder<T> {
-        let has_from = self.vertices.contains_key(&from);
-        let has_to = self.vertices.contains_key(&to);
+    fn _insert_edge(builder: &mut GraphBuilder<T>, from: &T, to: &T) {
+        let has_from = builder.vertices.contains_key(&from);
+        let has_to = builder.vertices.contains_key(&to);
 
         if !has_from || !has_to {
-            return self;
+            return;
         }
 
-        if let Some(from_vert) = self.vertices.get_mut(&from) {
+        if let Some(from_vert) = builder.vertices.get_mut(&from) {
             from_vert.insert(Node(to.clone()));
         }
 
-        if let Some(to_vert) = self.vertices.get_mut(&to) {
+        if let Some(to_vert) = builder.vertices.get_mut(&to) {
             to_vert.insert(Node(from.clone()));
         }
+    }
+
+    pub fn insert_edge(mut self, from: T, to: T) -> GraphBuilder<T> {
+        GraphBuilder::<T>::_insert_edge(&mut self, &from, &to);
 
         self
     }
 
-    fn from_vertices(mut self, nodes: Vec<T>) -> GraphBuilder<T> {
+    pub fn from_vertices(nodes: Vec<T>) -> GraphBuilder<T> {
+        let mut graph = GraphBuilder::<T>::new();
+
         for node in nodes {
-            self.vertices.insert(node, HashSet::new());
+            GraphBuilder::<T>::_insert_node(&mut graph, node);
         }
 
-        self
+        graph
     }
 
-    fn build(self) -> Graph<T> {
+    pub fn build(self) -> Graph<T> {
         Graph {
             vertices: self.vertices,
         }
